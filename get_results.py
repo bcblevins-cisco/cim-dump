5d760623c069eb53b835135f6cbc917dd5abb42c
Beau Blevins (beblevin) <beblevin@cisco.com>
2025-09-03 16:51:59 -0400

    Main data retrieval logic implemented

--- get_results.py
+++ get_results.py
@@ -1,5 +1,5 @@
-from datetime import datetime, timezone
 from dotenv import dotenv_values
+import requests
 
 
 # ============================================================================================
@@ -16,3 +16,18 @@ RUN_END_URL = ENV["RUN_END_URL"]
 
 # ============================================================================================
 
+# region Fetch Pipelines
+
+ids = []
+
+for p in PROJECT_IDS:
+    for i in range(4): # limit to 4 pages of results for testing
+        response = requests.get(f"{PIPELINES_URL}/{p}/{i}/ids")
+        data = response.json()
+        if len(data['pipeline_ids']) == 0:
+            print(f"End of ID groups for project {p}\n")
+            break
+        print(f"Pipeline group {i} for project {p} captured")
+        ids.extend(data['pipeline_ids'])
+
+print(f"\nCaptured a total of {len(ids)} pipeline IDs.")
